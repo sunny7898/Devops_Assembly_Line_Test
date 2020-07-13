ffdbd85886c2dc8dfcea6903b400eb60babf2cea
Sunny <sungoswami07@gmail.com>
2020-07-13 21:09:52 +0530

    BuildJob

---
+++ Post-job-status.groovy
@@ -0,0 +1,35 @@
+pipeline {
+	agent any
+	stages {
+		 stage("Dev") {
+			steps {
+				build job: 'job_Dev'			
+			}
+		}
+		stage("Test") {
+			steps {
+				build job: 'job_Test'
+				timeout(time: 1, unit: 'MINUTES') {
+					input message: 'Waiting for approval?'
+				}
+			}
+		}	
+		stage("Production") {
+			steps {
+				build job: 'job_Prod'
+			}
+		}
+	}
+	post {
+		success {
+			sh echo "Success...."
+		}
+		failure {
+			sh echo "Success...."
+		}
+		always {
+			sh echo "Success...."
+		}
+}
+
+
